aa185061ea9dcfbfcae53b138cbdd3d986d5eb8c
Yuvraj-Dhepe <yuvi.kiit@gmail.com>
2024-01-03 08:48:38 +0100

    Started with Statistical Analysis

--- eda_nbs/plots.py
+++ eda_nbs/plots.py
@@ -211,9 +211,13 @@ def create_line_plot(filtered_df, title):
     traces = []
     for channel in filtered_df['channelTitle'].unique():
         channel_data = filtered_df[filtered_df['channelTitle'] == channel]
+
+        # Aggregate data by summing up views for each month
+        aggregated_data = channel_data.groupby('publishingYear').agg({'viewCount': 'sum'}).reset_index()
+
         trace = go.Scatter(
-            x=channel_data['publishingYear'],
-            y=channel_data['viewCount'],
+            x=aggregated_data['publishingYear'],
+            y=aggregated_data['viewCount'],
             mode='lines+markers',
             name=channel
         )
@@ -227,29 +231,45 @@ def create_line_plot(filtered_df, title):
 
     return {'data': traces, 'layout': layout}
 
-def multi_plots(df, channel_titles):
+def dynamic_plots(df, channel_titles):
     # Create JupyterDash app with suppress_callback_exceptions=True
-    app = dash.Dash(__name__)
-    channel_options = [{'label': channel, 'value': channel} for channel in channel_titles]
-    year_options = [{'label': year, 'value': year} for year in df['publishingYear'].unique()]
+    app = dash.Dash(__name__, suppress_callback_exceptions=True)
+    pop_channels = [{'label': title, 'value': title} for title in df[df['pop_unpop'] == 1]['channelTitle'].unique()]
+    unpop_channels = [{'label': title, 'value': title} for title in df[df['pop_unpop'] == 0]['channelTitle'].unique()]
     # Layout of the app
     app.layout = html.Div([
         html.H1("Monthly and Yearly Views by Channel"),
-        dcc.Checklist(
-            id='channel-toggle',
-            options=channel_options,
-            value=[channel_options[0]['value']],  # Default to the first channel
-            inline = True,
-            style={'width': '50%', 'columnCount': 6}
-        ),
-        dcc.Dropdown(
-            id='year-dropdown',
-            options=year_options,
-            value=year_options[0]['value'],  # Default to the first year
-            multi=False,
-            style={'width': '50%'}
+        
+        html.Div([
+            html.H3("Select Channels"),
             
-        ),
+            # Checklists for Popular and Unpopular Channels
+            dcc.Checklist(
+                id='channel-checklist-popular',
+                options=pop_channels,
+                value=[pop_channels[0]['value']],  # Default to the first channel
+                inline=True,
+                style={'width': '50%', 'columnCount': 3, 'rowCount': 2, 'marginLeft': 20}
+            ),
+            
+            dcc.Checklist(
+                id='channel-checklist-unpopular',
+                options=unpop_channels,
+                value=[unpop_channels[0]['value']],  # Default to the first channel
+                inline=True,
+                style={'width': '50%', 'columnCount': 3, 'rowCount': 2, 'marginLeft': 20}
+            ),
+        ]),
+        html.Div([
+            html.H3("Select Years"),
+            dcc.Checklist(
+                id='year-checklist-bar-plots',
+                options=[{'label': year, 'value': year} for year in df['publishingYear'].unique()],
+                value=df['publishingYear'].unique(),  # Default to all years
+                inline=True,
+                style={'width': '50%', 'columnCount': 6, 'rowCount': 3, 'marginTop':20}
+            ),
+        ]),
         dcc.Graph(id='bar-plot-a'),
         dcc.Graph(id='bar-plot-b'),
         dcc.Graph(id='line-plot-a'),
@@ -261,19 +281,16 @@ def multi_plots(df, channel_titles):
     @app.callback(
         [Output('bar-plot-a', 'figure'),
          Output('bar-plot-b', 'figure')],
-        [Input('channel-toggle', 'value'),
-         Input('year-dropdown', 'value')],
-        
+        [Input('channel-checklist-popular', 'value'),
+         Input('channel-checklist-unpopular', 'value'),
+         Input('year-checklist-bar-plots', 'value')]
     )
-    def update_bar_plots(selected_channels):
-        popular_channels = df[df['pop_unpop'] == 1]
-        unpopular_channels = df[df['pop_unpop'] == 0]
+    def update_bar_plots(selected_channels, selected_years):
+        filtered_df_popular = df[(df['pop_unpop'] == 1) & (df['channelTitle'].isin(selected_channels)) & (df['publishingYear'].isin(selected_years))]
+        bar_plot_a = create_bar_plot(filtered_df_popular, title=f'Monthly Views for Popular Channels')
 
-        bar_plot_a = create_bar_plot(popular_channels[popular_channels['channelTitle'].isin(selected_channels)],
-                                     title='Monthly Views for Popular Channels')
-
-        bar_plot_b = create_bar_plot(unpopular_channels[unpopular_channels['channelTitle'].isin(selected_channels)],
-                                     title='Monthly Views for Unpopular Channels')
+        filtered_df_unpopular = df[(df['pop_unpop'] == 0) & (df['channelTitle'].isin(selected_channels)) & (df['publishingYear'].isin(selected_years))]
+        bar_plot_b = create_bar_plot(filtered_df_unpopular, title=f'Monthly Views for Unpopular Channels')
 
         return bar_plot_a, bar_plot_b
 
@@ -282,20 +299,17 @@ def multi_plots(df, channel_titles):
         [Output('line-plot-a', 'figure'),
          Output('line-plot-b', 'figure'),
          Output('line-plot-c', 'figure')],
-        [Input('channel-toggle', 'value')]
+        [Input('channel-checklist', 'value')]
     )
     def update_line_plots(selected_channels):
-        popular_channels = df[df['pop_unpop'] == 1]
-        unpopular_channels = df[df['pop_unpop'] == 0]
+        filtered_df_popular = df[(df['pop_unpop'] == 1) & (df['channelTitle'].isin(selected_channels))]
+        line_plot_a = create_line_plot(filtered_df_popular, title=f'Yearly Trend for Popular Channels')
 
-        line_plot_a = create_line_plot(popular_channels[popular_channels['channelTitle'].isin(selected_channels)],
-                                       title='Yearly Trend for Popular Channels')
+        filtered_df_unpopular = df[(df['pop_unpop'] == 0) & (df['channelTitle'].isin(selected_channels))]
+        line_plot_b = create_line_plot(filtered_df_unpopular, title=f'Yearly Trend for Unpopular Channels')
 
-        line_plot_b = create_line_plot(unpopular_channels[unpopular_channels['channelTitle'].isin(selected_channels)],
-                                       title='Yearly Trend for Unpopular Channels')
-
-        line_plot_c = create_line_plot(df[df['channelTitle'].isin(selected_channels)],
-                                       title='Yearly Trend for All Selected Channels')
+        all_channels = df[df['channelTitle'].isin(selected_channels)]
+        line_plot_c = create_line_plot(all_channels, title=f'Yearly Trend for All Selected Channels')
 
         return line_plot_a, line_plot_b, line_plot_c
 
